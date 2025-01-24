f37cc866ab3161870f6a797fb44e9063fa6be371
racostyle <cekcar@gmail.com>
2025-01-24 18:28:16 +0100

    Adapter modifications

---
+++ Automation/ConfigurationAdapter/VisualTreeAdapter.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows.Media;
+
+namespace Automation.ConfigurationAdapter
+{
+    /// <summary>
+    /// naming convention prefixes: tb - textbox, rtb - RichTextBox, chb - CheckBox, cbb - ComboBox, rbtn - RadioButton
+    /// Example: tbTextBox will come out from GetControlNameWithoutPrefix as TextBox
+    /// </summary>
+    public class VisualTreeAdapter
+    {
+        public bool SavedByAdapter { get; private set; } = false;
+
+        private readonly IVisualHandler[] _visualHandlers;
+
+        public VisualTreeAdapter(IVisualHandler[] visualHandlers)
+        {
+            _visualHandlers = visualHandlers;
+        }
+
+        public Dictionary<string, string> Pack(Visual visualTree)
+        {
+            var recognized = GetAllRecognizedVisuals(visualTree);
+
+            var config = new Dictionary<string, string>();
+
+            foreach (var visual in recognized)
+            {
+                foreach (var handler in _visualHandlers)
+                {
+                    if (handler.DoesMatchTo(visual))
+                        config.Add(handler.GetVisualNameWithoutPrefix(visual), handler.GetVisualValue(visual));
+                }
+            }
+
+            return config;
+        }
+
+        public Dictionary<string, string> Unpack(Visual visualTree, Dictionary<string, string> config)
+        {
+            if (config == null)
+                return new Dictionary<string, string>();
+
+            SavedByAdapter = true;
+
+            var recognized = GetAllRecognizedVisuals(visualTree);
+
+            foreach (var visual in recognized)
+            {
+                foreach (var handler in _visualHandlers)
+                {
+                    var name = handler.GetVisualNameWithoutPrefix(visual);
+                    if (handler.DoesMatchTo(visual) && config.ContainsKey(name))
+                    {
+                        handler.AssignValueToVisual(visual, config[name]);
+                    }
+                }
+            }
+
+            SavedByAdapter = false;
+            return config;
+        }
+
+        private List<Visual> GetAllRecognizedVisuals(Visual visualTree)
+        {
+            var visuals = FetchAllVisuals(visualTree);
+            var recognized = new List<Visual>();
+
+            foreach (var visual in visuals)
+            {
+                if (_visualHandlers.Any(x => x.DoesMatchTo(visual)))
+                    recognized.Add(visual);
+            }
+            return recognized;
+        }
+
+        public List<Visual> FetchAllVisuals(Visual visualTree)
+        {
+            var visuals = new List<Visual>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(visualTree); i++)
+            {
+                Visual childVisual = (Visual)VisualTreeHelper.GetChild(visualTree, i);
+                visuals.Add(childVisual);
+                // Recurse into children
+                visuals.AddRange(FetchAllVisuals(childVisual));
+            }
+            return visuals;
+        }
+
+    }
+}
--- Automation/MainWindow.xaml.cs
+++ Automation/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Text.Json;
 using System.Windows;
-using System.Windows.Media;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Automation
@@ -15,107 +14,43 @@ namespace Automation
         public MainWindow()
         {
             InitializeComponent();
-            this.Loaded += MainWindow_Loaded; ;
+            this.Loaded += MainWindow_Loaded;
+            this.Closed += MainWindow_Closed;
+        }
+
+        #region CLOSED & LOADED HANDLERS
+        private void MainWindow_Closed(object? sender, EventArgs e)
+        {
+            var adapter = BuildHandler();
+
+            var config = adapter.Pack(this);
+
+            var text = JsonSerializer.Serialize(config);
+            File.WriteAllText("appsettings.json", text);
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             Task.Delay(100);
 
+            var adapter = BuildHandler();
+
+            var json = File.ReadAllText("appsettings.json");
+            var config = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+
+            adapter.Unpack(this, config);
+
+        }
+
+        private VisualTreeAdapter BuildHandler()
+        {
             var handlers = new IVisualHandler[]
             {
                  new Handler_TextBox()
             };
 
-            var handler = new VisualTreeAdapter(handlers);
-
-            //var config = handler.Pack(this);
-
-            //var text = JsonSerializer.Serialize(config);
-            //File.WriteAllText("appsettings.json", text);
-
-            var json = File.ReadAllText("appsettings.json");
-            var config = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
-
-            handler.Unpack(this, config);
-
+            return new VisualTreeAdapter(handlers);
         }
-    }
-
-    public class VisualTreeAdapter
-    {
-        private readonly IVisualHandler[] _visualHandlers;
-
-        public VisualTreeAdapter(IVisualHandler[] visualHandlers)
-        {
-            _visualHandlers = visualHandlers;
-        }
-
-        public Dictionary<string, string> Pack(Visual visualTree)
-        {
-            var visuals = FetchAllVisuals(visualTree);
-
-            var config = new Dictionary<string, string>();
-
-            foreach (var visual in visuals)
-            {
-                foreach (var handler in _visualHandlers)
-                {
-                    if (handler.DoesMatchTo(visual))
-                        config.Add(handler.GetVisualNameWithoutPrefix(visual), handler.GetVisualValue(visual));
-                }
-            }
-
-            return config;
-        }
-
-        public Dictionary<string, string> Unpack(Visual visualTree, Dictionary<string,string> config)
-        {
-            if (config == null)
-                return new Dictionary<string, string>();
-
-            var recognized = GetAllRecognizedVisuals(visualTree);
-
-            foreach (var visual in recognized)
-            {
-                foreach (var handler in _visualHandlers)
-                {
-                    var name = handler.GetVisualNameWithoutPrefix(visual);
-                    if (handler.DoesMatchTo(visual) && config.ContainsKey(name))
-                    {
-                        handler.AssignValueToVisual(visual, config[name]);
-                    }
-                }
-            }
-
-            return config;
-        }
-
-        private List<Visual> GetAllRecognizedVisuals(Visual visualTree)
-        {
-            var visuals = FetchAllVisuals(visualTree);
-            var recognized = new List<Visual>();
-
-            foreach (var visual in visuals)
-            {
-                if (_visualHandlers.Any(x => x.DoesMatchTo(visual)))
-                    recognized.Add(visual);
-            }
-            return recognized;
-        }
-
-        public List<Visual> FetchAllVisuals(Visual visualTree)
-        {
-            var visuals = new List<Visual>();
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(visualTree); i++)
-            {
-                Visual childVisual = (Visual)VisualTreeHelper.GetChild(visualTree, i);
-                visuals.Add(childVisual);
-                // Recurse into children
-                visuals.AddRange(FetchAllVisuals(childVisual));
-            }
-            return visuals;
-        }
-
+        #endregion
     }
 }
