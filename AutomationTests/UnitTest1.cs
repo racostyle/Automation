705ec4438bdc6a772b346c24283f1c6e10772298
racostyle <cekcar@gmail.com>
2025-04-03 15:39:01 +0200

    TestingPrep

---
+++ AutomationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AutomationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
