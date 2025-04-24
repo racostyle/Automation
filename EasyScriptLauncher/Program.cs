bde399abdf92ea1325b96c172eb9b94019bf7eaa
racostyle <cekcar@gmail.com>
2025-04-24 12:00:31 +0200

    EasyScriptLauncher update

--- EasyScriptLauncher/Program.cs
+++ EasyScriptLauncher/Program.cs
@@ -13,48 +13,48 @@ namespace EasyScriptLauncher
         static void Main(string[] args)
         {
             var info = new Info(new Logger());
+            var config = new SettingsLoader().LoadSettings(Path.Combine(Directory.GetCurrentDirectory(), SETTINGS_FILE));
 
-            try
+            if (!Directory.Exists(config.ScriptsFolder))
             {
-                var config = new SettingsLoader().LoadSettings(Path.Combine(Directory.GetCurrentDirectory(), SETTINGS_FILE));
-                var processLauncher = new ProcessLauncher(info, config);
+                info.FillTheSettings(Path.Combine(Directory.GetCurrentDirectory(), SETTINGS_FILE));
+                Environment.Exit(1);
+            }
 
-                if (!Directory.Exists(config.ScriptsFolder))
-                {
-                    info.FillTheSettings(Path.Combine(Directory.GetCurrentDirectory(), SETTINGS_FILE));
-                    Environment.Exit(1);
-                }
+            string[] scripts;
+            if (config.SearchForScriptsRecursively)
+                scripts = Directory.GetFiles(config.ScriptsFolder, "*.ps1", SearchOption.AllDirectories);
+            else
+                scripts = Directory.GetFiles(config.ScriptsFolder, "*.ps1", SearchOption.TopDirectoryOnly);
 
-                string[] scripts;
-                if (config.SearchForScriptsRecursively)
-                    scripts = Directory.GetFiles(config.ScriptsFolder, "*.ps1", SearchOption.AllDirectories);
-                else
-                    scripts = Directory.GetFiles(config.ScriptsFolder, "*.ps1", SearchOption.TopDirectoryOnly);
+            if (scripts.Length == 0)
+            {
+                info.NoScriptsFound(config.ScriptsFolder, config.SearchForScriptsRecursively);
+                Environment.Exit(1);
+            }
 
-                if (scripts.Length == 0)
-                {
-                    info.NoScriptsFound(config.ScriptsFolder, config.SearchForScriptsRecursively);
-                    Environment.Exit(1);
-                }
+            if (config.DelayInMils > 0)
+            {
+                Console.WriteLine("Waiting for environment to set up!");
+                Console.WriteLine($"Sleeping for {config.DelayInMils / 1000} seconds...");
+                Thread.Sleep(config.DelayInMils);
+            }
 
-                if (config.DelayInMils > 0)
-                {
-                    Console.WriteLine("Waiting for environment to set up!");
-                    Console.WriteLine($"Sleeping for {config.DelayInMils/1000} seconds...");
-                    Thread.Sleep(config.DelayInMils);
-                }
+            var processLauncher = new ProcessLauncher(info, config);
 
-                foreach (var script in scripts)
+            foreach (var script in scripts)
+            {
+                try
                 {
                     info.StartingScript(script);
                     processLauncher.StartProcess(script, config);
                 }
+                catch (Exception ex)
+                {
+                    info.GenericError(ex.Message);
+                }
             }
-            catch (Exception ex)
-            {
-                info.GenericError(ex.Message);
-                Environment.Exit(1);
-            }
+
             info.Done();
         }
     }
